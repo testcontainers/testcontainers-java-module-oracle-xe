6645e560590a114b1f400843e834d76b4a26584a
Richard North <rich.north@gmail.com>
2017-07-01 19:38:22 +0100

    Update Maven release signing variables

--- release/deploy.sh
+++ release/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-openssl aes-256-cbc -K $encrypted_4ed9d1624ee2_key -iv $encrypted_4ed9d1624ee2_iv -in release/codesigning.asc.enc -out release/codesigning.asc -d
+openssl aes-256-cbc -K $encrypted_350aba14307b_key -iv $encrypted_350aba14307b_iv -in release/codesigning.asc.enc -out release/codesigning.asc -d
 gpg --fast-import release/codesigning.asc
 
 mvn versions:set -DnewVersion=$TRAVIS_TAG
