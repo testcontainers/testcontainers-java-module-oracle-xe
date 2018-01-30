bf30c4f6435f8bf1858ab9114bfd056930d22c09
Richard North <rich.north@gmail.com>
2018-01-30 19:36:24 +0000

    Disable `useSSL` query param for Oracle/SQL Server modules Temporary fix for testcontainers/testcontainers-java#568

--- src/main/java/org/testcontainers/containers/OracleContainer.java
+++ src/main/java/org/testcontainers/containers/OracleContainer.java
@@ -1,6 +1,15 @@
 package org.testcontainers.containers;
 
+import org.rnorth.ducttape.ratelimits.RateLimiter;
+import org.rnorth.ducttape.ratelimits.RateLimiterBuilder;
+import org.rnorth.ducttape.unreliables.Unreliables;
 import org.testcontainers.utility.TestcontainersConfiguration;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author gusohal
@@ -12,6 +21,11 @@ public class OracleContainer<SELF extends JdbcDatabaseContainer<SELF>> extends J
             .getProperties().getProperty("oracle.container.image","wnameless/oracle-xe-11g");
     private static final int ORACLE_PORT = 1521;
     private static final int APEX_HTTP_PORT = 8080;
+
+    private static final RateLimiter DB_CONNECT_RATE_LIMIT = RateLimiterBuilder.newBuilder()
+            .withRate(10, TimeUnit.SECONDS)
+            .withConstantThroughput()
+            .build();
 
     public OracleContainer() {
         super(IMAGE + ":latest");
@@ -28,7 +42,6 @@ public class OracleContainer<SELF extends JdbcDatabaseContainer<SELF>> extends J
 
     @Override
     protected void configure() {
-
         addExposedPorts(ORACLE_PORT, APEX_HTTP_PORT);
     }
 
@@ -69,5 +82,22 @@ public class OracleContainer<SELF extends JdbcDatabaseContainer<SELF>> extends J
     @Override
     public String getTestQueryString() {
         return "SELECT 1 FROM DUAL";
+    }
+
+    // TODO: Replace with permanent solution to https://github.com/testcontainers/testcontainers-java/issues/568
+    @Override
+    public Connection createConnection(String queryString) throws SQLException {
+        final Properties info = new Properties();
+        info.put("user", this.getUsername());
+        info.put("password", this.getPassword());
+        final String url = this.getJdbcUrl() + queryString;
+
+        final Driver jdbcDriverInstance = getJdbcDriverInstance();
+
+        try {
+            return Unreliables.retryUntilSuccess(120, TimeUnit.SECONDS, () -> jdbcDriverInstance.connect(url, info));
+        } catch (Exception e) {
+            throw new SQLException("Could not create new connection", e);
+        }
     }
 }
